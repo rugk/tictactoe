4f17b36bbffcb2c880a1815b5235da478ec4a6fb
rugk <rugk@posteo.de>
2018-01-08 13:06:47 +0100

    Projektdateien hinzufügen.

---
+++ TickTackTo/GameForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickTackTo
+{
+    public partial class GameForm : Form
+    {
+        private Game game;
+
+        // box 
+        public PictureBox[,] PicField { get; set; } = new PictureBox[3, 3];
+
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Prepares the UI/form to start with a game.
+        /// </summary>
+        /// <param name="gameInstance"></param>
+        public void StartGame(Game gameInstance)
+        {
+            this.game = gameInstance;
+
+            // initialize/reset PicField
+            PicField = new PictureBox[3, 3] {
+                { pic11, pic12, pic13 },
+                { pic21, pic22, pic23 },
+                { pic31, pic32, pic33 },
+            };
+            
+        }
+
+        private void PicClicked(object sender, EventArgs e)
+        {
+            // we know our sender
+            PictureBox clickedPic = (PictureBox) sender;
+            
+            // find out position of button
+            int[] pos = getPositionOfPic(clickedPic);
+
+            // TODO: check if field can be clicked
+            if (this.game.CanSelectPicture(pos))
+            {
+                // change image
+                if (this.game.CurrentPlayer == Player.PlayerX)
+                {
+                    clickedPic.Image = Properties.Resources.picX;
+                }
+                else
+                {
+                    clickedPic.Image = Properties.Resources.picO;
+                }
+                
+                // change internal data
+                this.game.SelectPicture(pos);
+                this.game.SwitchPlayer();
+            }
+        }
+
+        public void ShowWinGame(Player whoWon, int? line, int? column, int? diagonal)
+        {
+            string whereWon = "";
+
+            if (line != null)
+            {
+                whereWon += String.Format("line {0}, ", line + 1);
+            }
+            if (column != null)
+            {
+                whereWon += String.Format("column {0}, ", column + 1);
+            }
+            if (diagonal != null)
+            {
+                whereWon += "diagonal direction, ";
+            }
+
+            // remove last two characters, which were appended
+            Debug.WriteLine(whereWon);
+            whereWon = whereWon.Substring(0, whereWon.Length - 2);
+            Debug.WriteLine(whereWon);
+
+            // replace last comma with and for good style
+            whereWon = Program.ReplaceLastOccurrence(whereWon, ", ", " and ");
+
+            this.ShowMessage("{0} won in {1}!", Program.FirstLetterToUpper(this.game.GetNameOfPlayer(whoWon)), whereWon);
+        }
+
+        private int[] getPositionOfPic(PictureBox elem)
+        {
+            // iterate through fields
+            for (int column = 0; column < this.PicField.GetLength(0); column++)
+            {
+                for (int line = 0; line < this.PicField.GetLength(0); line++)
+                {
+                    Debug.WriteLine("{0} == {1} ??", elem, this.PicField[column, line]);
+                    // and compare PictureBox to see whether it's the one, which we need
+                    if (elem.Equals(this.PicField[column, line]))
+                    {
+                        Debug.WriteLine("getPositionOfPic: line {0}, column {1} = {2}", line, column, this.PicField[column, line]);
+                        return new int[2] { line, column };
+                    }
+                }
+            }
+
+            throw new ArgumentOutOfRangeException("could not find picture");
+        }
+
+        /// <summary>
+        /// Custom message function, so message can be shown in a different/other way in the future
+        /// </summary>
+        /// <param name="message">the message to show</param>
+        /// <param name="contentArray">all params to pass to String.Format</param>
+        public void ShowMessage(string message, params object[] contentArray)
+        {
+            MessageBox.Show(this, String.Format(message, contentArray));
+        }
+
+        /// <summary>
+        /// show game beginenr message
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameForm_Shown(object sender, EventArgs e)
+        {
+            this.ShowMessage("{0} begins", Program.FirstLetterToUpper(this.game.GetNameOfPlayer(this.game.CurrentPlayer)));
+        }
+    }
+}
---
+++ TickTackTo/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickTackTo
+{
+    static class Program
+    {
+        const 
+
+        /// <summary>
+        /// The main entry got the application
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // create window
+            GameForm gameWindow = new GameForm();
+
+            Debug.WriteLine("Start app.");
+
+            Game newGame = new Game(gameWindow);
+            newGame.StartGame(Player.PlayerX);
+            gameWindow.StartGame(newGame);
+
+            // show form
+            Application.Run(gameWindow);
+        }
+        /// <summary>
+        /// Helper function, thanks https://stackoverflow.com/a/4135491
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+
+        /// <summary>
+        /// replace the last occurence of a string, thanks https://stackoverflow.com/a/14826068
+        /// </summary>
+        /// <param name="Source"></param>
+        /// <param name="Find"></param>
+        /// <param name="Replace"></param>
+        /// <returns></returns>
+        public static string ReplaceLastOccurrence(string Source, string Find, string Replace)
+        {
+            int place = Source.LastIndexOf(Find);
+
+            if (place == -1)
+                return Source;
+
+            string result = Source.Remove(place, Find.Length).Insert(place, Replace);
+            return result;
+        }
+    }
+}
