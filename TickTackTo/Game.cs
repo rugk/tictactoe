4c585f99cccdbab6afa0f402648a2f4e0102dc31
rugk <rugk@posteo.de>
2018-01-08 13:06:47 +0100

    Projektdateien hinzufügen.

---
+++ TickTackTo/Game.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickTackTo
+{
+    public enum Player
+    {
+        PlayerNull, // field not played/unspecified player
+        PlayerX,
+        PlayerO
+    }
+
+    public class Game
+    {
+        public Player CurrentPlayer { get; protected set; }
+        private Player[,] Field = new Player[3, 3];
+
+        private GameForm gameWindow;
+ 
+        /// <summary>
+        /// simple constructor
+        /// </summary>
+        /// <param name="gameWindow">the window where the game should be "attached to"/executed</param>
+        public Game(GameForm gameWindow)
+        {
+            this.gameWindow = gameWindow;
+        }
+
+        /// <summary>
+        /// gets into form loop
+        /// </summary>
+        public void StartGame(Player startPlayer)
+        {
+            // TODO: random begin
+            this.CurrentPlayer = startPlayer;
+
+            // erase player array, so we can start with an empty one, if needed
+            this.Field = new Player[3, 3];
+        }
+
+        /// <summary>
+        /// wether the user can actually select that picture
+        /// </summary>
+        /// <param name="pos">The position as a two-dimensional array:
+        ///     0=x=column;
+        ///     1=y=line
+        /// </param>
+        public bool CanSelectPicture(int[] pos)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pos">The position as a two-dimensional array:
+        ///     0=x=column;
+        ///     1=y=line
+        /// </param>
+        public void SelectPicture(int[] pos)
+        {
+            Debug.WriteLine("SelectPicture: column {0}, line {1}", pos[0], pos[1]);
+
+            this.Field[pos[0], pos[1]] = this.CurrentPlayer;
+
+            // automatically check game result as the staten can change at each picture change
+            CheckGameEnd();
+        }
+
+        public void SwitchPlayer()
+        {
+            if (this.CurrentPlayer == Player.PlayerO)
+            {
+                this.CurrentPlayer = Player.PlayerX;
+                return;
+            } else if (this.CurrentPlayer == Player.PlayerX)
+            {
+                this.CurrentPlayer = Player.PlayerO;
+                return;
+            }
+
+            // else
+            throw new InvalidOperationException("player state invalid");
+        }
+
+        public void CheckGameEnd()
+        {
+            GameResult result = GetGameResult();
+
+            // early return if noone won
+            if (result.Line == null && result.Column == null && result.Diagonal == null)
+            {
+                return;
+            }
+
+            // show winner message
+            this.gameWindow.ShowWinGame((Player)result.Winner, result.Line, result.Column, result.Diagonal);
+
+            // start new game?
+            this.StartGame();
+        }
+
+        public struct GameResult
+        {
+            public Player? Winner { get; set; }
+            public int? Line { get; set; }
+            public int? Column { get; set; }
+            public int? Diagonal { get; set; }
+            public GameResult(Player? winner, int? line, int? column, int? diagonal)
+            {
+                this.Winner = winner;
+                this.Line = line;
+                this.Column = column;
+                this.Diagonal = diagonal;
+            }
+        }
+        
+        public string GetNameOfPlayer(Player player)
+        {
+            // also possible: return player.ToString();
+
+            switch (player)
+            {
+                // break not needed as early return is used here and so the break would never be reached anyway
+                case Player.PlayerX:
+                    return "player 1";
+                case Player.PlayerO:
+                    return "player 2";
+                // PlayerNull and any other value does not need to return something useful
+                case Player.PlayerNull:
+                default:
+                    return "unknown player";
+            }
+        }
+
+        /// <summary>
+        /// Return the lines, columns and diagonal elements where a player won, if they won...
+        /// </summary>
+        private GameResult GetGameResult()
+        {
+            int numLines = this.Field.GetLength(1);
+            int numColumns = this.Field.GetLength(0);
+
+            // intialize var with first item as this is always the first reference
+            Player lastLineElem = this.Field[0, 0];
+
+            // line, column, diagonal
+            GameResult result = new GameResult();
+
+            // loop through lines in order to find marked ones
+            for (int line = 0; line < numLines; line++)
+            {
+                // new line
+                lastLineElem = this.Field[0, line];
+
+                Debug.WriteLine("testing line {0} of {1}", line, numLines-1);
+                for (int column = 0; column < numColumns; column++)
+                {
+                    Player currElement = this.Field[column, line];
+                    Debug.WriteLine("CheckGameResult: column {0}, line {1} = {2}", column, line, currElement);
+
+                    // break loop if no marked line can be found
+                    if (currElement == Player.PlayerNull || currElement != lastLineElem)
+                    {
+                        // nullify last line element to indicate break
+                        lastLineElem = Player.PlayerNull;
+                        break;
+                    }
+                }
+
+                // if we are at the end and we found no different value in line, someone won
+                if (lastLineElem != Player.PlayerNull)
+                {
+                    Debug.WriteLine("SUCCESS: found line {0}", line);
+                    result.Winner = lastLineElem;
+                    result.Line = line;
+                    // only one line can be matching when properly checked -> do not need to test more
+                    break;
+                }
+            }
+
+            // again first item first
+            Player lastColElem = this.Field[0, 0];
+
+            // loop through columns in order to find marked ones
+            for (int column = 0; column < numColumns; column++)
+            {
+                // new line
+                lastColElem = this.Field[column, 0];
+
+                Debug.WriteLine("testing column {0} of {1}", column, numColumns-1);
+                for (int line = 0; line < numLines; line++)
+                {
+                    Player currElement = this.Field[column, line];
+                    Debug.WriteLine("CheckGameResult: column {0}, line {1} = {2}", column, line, currElement);
+
+                    // break loop if no marked column can be found
+                    if (currElement == Player.PlayerNull || currElement != lastColElem)
+                    {
+                        // nullify last line element to indicate break
+                        lastColElem = Player.PlayerNull;
+                        break;
+                    }
+                }
+
+                // if we are at the end and we found no different value in column, someone won
+                if (lastColElem != Player.PlayerNull)
+                {
+                    Debug.WriteLine("SUCCESS: found column {0}", column);
+                    result.Winner = lastColElem;
+                    result.Column = column;
+                    // only one line can be matching when properly checked -> do not need to test more
+                    break;
+                }
+            }
+
+            // again first item first
+            Player lastDia1Elem = this.Field[0, 0];
+
+            // check diagonal 1
+            for (int dia = 0; dia < numColumns; dia++)
+            {
+                Player currElement = this.Field[dia, dia];
+                Debug.WriteLine("CheckGameResult: dia1 {0}, line {1} = {2}", dia, dia, currElement);
+
+                // break loop if no marked element can be found
+                if (currElement == Player.PlayerNull || currElement != lastDia1Elem)
+                {
+                    // nullify last line element to indicate break
+                    lastDia1Elem = Player.PlayerNull;
+                    break;
+                }
+            }
+
+            // if we are at the end and we found no different value in dia, someone won
+            if (lastDia1Elem != Player.PlayerNull)
+            {
+                Debug.WriteLine("SUCCESS: found result in diagonal 1");
+                result.Winner = lastDia1Elem;
+                result.Diagonal = 1;
+            }
+
+            // this time we start from upper right, so first element is this...
+            Player lastDia2Elem = this.Field[2, 0];
+
+            // check diagonal 2
+            for (int dia1 = 0; dia1 < numColumns; dia1++)
+            {
+                // the second diagonale is the "reverse" from dia1
+                int dia2 = 2 - dia1;
+
+                Player currElement = this.Field[dia1, dia2];
+                Debug.WriteLine("CheckGameResult: dia2 {0}, line {1} = {2}", dia1, dia2, currElement);
+
+                // break loop if no marked element can be found
+                if (currElement == Player.PlayerNull || currElement != lastDia2Elem)
+                {
+                    // nullify last line element to indicate break
+                    lastDia2Elem = Player.PlayerNull;
+                    break;
+                }
+            }
+
+            // if we are at the end and we found no different value in dia, someone won
+            if (lastDia2Elem != Player.PlayerNull)
+            {
+                Debug.WriteLine("SUCCESS: found result in diagonal 2");
+                result.Winner = lastDia2Elem;
+                result.Diagonal = 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TickTackTo/GameForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace TickTackTo
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pic33 = new System.Windows.Forms.PictureBox();
+            this.pic32 = new System.Windows.Forms.PictureBox();
+            this.pic31 = new System.Windows.Forms.PictureBox();
+            this.pic23 = new System.Windows.Forms.PictureBox();
+            this.pic22 = new System.Windows.Forms.PictureBox();
+            this.pic21 = new System.Windows.Forms.PictureBox();
+            this.pic13 = new System.Windows.Forms.PictureBox();
+            this.pic11 = new System.Windows.Forms.PictureBox();
+            this.pic12 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic33)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic31)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.pic33, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic32, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic31, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic23, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic22, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic21, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic13, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic11, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic12, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(382, 353);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pic33
+            // 
+            this.pic33.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic33.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic33.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic33.Location = new System.Drawing.Point(257, 237);
+            this.pic33.Name = "pic33";
+            this.pic33.Size = new System.Drawing.Size(122, 113);
+            this.pic33.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic33.TabIndex = 9;
+            this.pic33.TabStop = false;
+            this.pic33.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic32
+            // 
+            this.pic32.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic32.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic32.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic32.Location = new System.Drawing.Point(130, 237);
+            this.pic32.Name = "pic32";
+            this.pic32.Size = new System.Drawing.Size(121, 113);
+            this.pic32.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic32.TabIndex = 8;
+            this.pic32.TabStop = false;
+            this.pic32.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic31
+            // 
+            this.pic31.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic31.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic31.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic31.Location = new System.Drawing.Point(3, 237);
+            this.pic31.Name = "pic31";
+            this.pic31.Size = new System.Drawing.Size(121, 113);
+            this.pic31.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic31.TabIndex = 7;
+            this.pic31.TabStop = false;
+            this.pic31.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic23
+            // 
+            this.pic23.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic23.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic23.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic23.Location = new System.Drawing.Point(257, 120);
+            this.pic23.Name = "pic23";
+            this.pic23.Size = new System.Drawing.Size(122, 111);
+            this.pic23.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic23.TabIndex = 6;
+            this.pic23.TabStop = false;
+            this.pic23.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic22
+            // 
+            this.pic22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic22.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic22.Location = new System.Drawing.Point(130, 120);
+            this.pic22.Name = "pic22";
+            this.pic22.Size = new System.Drawing.Size(121, 111);
+            this.pic22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic22.TabIndex = 5;
+            this.pic22.TabStop = false;
+            this.pic22.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic21
+            // 
+            this.pic21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic21.Location = new System.Drawing.Point(3, 120);
+            this.pic21.Name = "pic21";
+            this.pic21.Size = new System.Drawing.Size(121, 111);
+            this.pic21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic21.TabIndex = 4;
+            this.pic21.TabStop = false;
+            this.pic21.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic13
+            // 
+            this.pic13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic13.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic13.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic13.Location = new System.Drawing.Point(257, 3);
+            this.pic13.Name = "pic13";
+            this.pic13.Size = new System.Drawing.Size(122, 111);
+            this.pic13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic13.TabIndex = 3;
+            this.pic13.TabStop = false;
+            this.pic13.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic11
+            // 
+            this.pic11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic11.Location = new System.Drawing.Point(3, 3);
+            this.pic11.Name = "pic11";
+            this.pic11.Size = new System.Drawing.Size(121, 111);
+            this.pic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic11.TabIndex = 2;
+            this.pic11.TabStop = false;
+            this.pic11.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // pic12
+            // 
+            this.pic12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pic12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pic12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic12.Location = new System.Drawing.Point(130, 3);
+            this.pic12.Name = "pic12";
+            this.pic12.Size = new System.Drawing.Size(121, 111);
+            this.pic12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic12.TabIndex = 1;
+            this.pic12.TabStop = false;
+            this.pic12.Click += new System.EventHandler(this.PicClicked);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "GameForm";
+            this.Text = "TickTackTo";
+            this.Shown += new System.EventHandler(this.GameForm_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic33)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic31)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pic33;
+        private System.Windows.Forms.PictureBox pic32;
+        private System.Windows.Forms.PictureBox pic31;
+        private System.Windows.Forms.PictureBox pic23;
+        private System.Windows.Forms.PictureBox pic22;
+        private System.Windows.Forms.PictureBox pic21;
+        private System.Windows.Forms.PictureBox pic13;
+        private System.Windows.Forms.PictureBox pic12;
+        private System.Windows.Forms.PictureBox pic11;
+    }
+}
+
