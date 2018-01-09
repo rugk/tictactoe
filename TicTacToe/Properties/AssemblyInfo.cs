4fc95995a2be77c195e6b3ee8014363f69252af2
rugk <rugk@posteo.de>
2018-01-09 14:57:09 +0100

    Correct name of project
    Now TicTacToe!!!

--- TicTacToe/Properties/AssemblyInfo.cs
+++ TicTacToe/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("TickTackTo")]
+[assembly: AssemblyTitle("TicTacToe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("***REMOVED***")]
-[assembly: AssemblyProduct("TickTackTo")]
+[assembly: AssemblyProduct("TicTacToe")]
 [assembly: AssemblyCopyright("***REMOVED***")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- TicTacToe/Properties/Resources.Designer.cs
+++ TicTacToe/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TickTackTo.Properties {
+namespace TicTacToe.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TickTackTo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TickTackTo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TicTacToe.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
